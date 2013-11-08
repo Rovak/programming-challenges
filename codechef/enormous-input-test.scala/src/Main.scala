ed1a9d2951f099055839087daf33bdc2339f9989
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-11-08 20:43:37 +0100

    enormous input test

---
+++ codechef/enormous-input-test.scala/src/Main.scala
@@ -0,0 +1,8 @@
+object Main {
+  def main(args: Array[String]) {
+    val input = readLine.split(" ")
+    val n = input(0).toInt
+    val k = input(1).toInt
+    println((1 to n).count(x => readInt % k == 0))
+  }
+}
