de267ef8085d3fcbfba732da025ba736d8c2abe6
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-11-08 20:43:32 +0100

    atm

---
+++ codechef/atm.scala/src/Main.scala
@@ -0,0 +1,12 @@
+/**
+ * http://www.codechef.com/problems/HS08TEST
+ */
+object Main {
+  def main(args: Array[String]) {
+    val input = readLine.split(" ")
+    val w = input(0).toInt
+    val b = input(1).toFloat
+    val t = b - w - 0.50
+    println(if ((w % 5 == 0) && t >= 0) "%.2f".format(t) else b)
+  }
+}
