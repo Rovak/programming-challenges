ef2e4dfe5beafd276e446656d20bae694a2c1046
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-11-21 23:18:50 +0100

    readLine => readInt

--- scala/src/main/scala/codechef/LifeTheUniverseAndEverything.scala
+++ scala/src/main/scala/codechef/LifeTheUniverseAndEverything.scala
@@ -2,6 +2,6 @@ package codechef
 
 object LifeTheUniverseAndEverything {
   def main(args: Array[String]) {
-    Iterator.continually(readLine()).takeWhile(_.toInt != 42).foreach(println(_))
+    Iterator.continually(readInt()).takeWhile(_ != 42).foreach(println(_))
   }
 }
